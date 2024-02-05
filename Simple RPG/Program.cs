faff72832cdeeeacdd2cbc265043d5e6598222e1
davidfreitas51 <152209400+davidfreitas51@users.noreply.github.com>
2024-02-05 20:18:56 -0300

    Console.WriteLine modified again

--- Simple RPG/Program.cs
+++ Simple RPG/Program.cs
@@ -26,7 +26,7 @@ namespace Simple_RPG
 
             Fight.Combat(player, stoneGuardian);
             //Places.Places.Dungeon(player, dungeonEntities);
-            Console.WriteLine("Where you wanna go?\n1 - Dungeon");
+            Console.WriteLine("Where you wanna go?\n-----\n1 - Dungeon");
         }
     }
 }
