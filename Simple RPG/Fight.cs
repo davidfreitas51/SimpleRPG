2bd5ba7845bd01f5b29b90fa8287e5287e268e4e
davidfreitas51 <152209400+davidfreitas51@users.noreply.github.com>
2024-02-07 21:05:06 -0300

    Back to shadowspeed, but with blocks working

--- Simple RPG/Fight.cs
+++ Simple RPG/Fight.cs
@@ -6,7 +6,7 @@ namespace Combat
 {
     public static class Fight
     {
-        public static int TestAccuracy(int accuracy)
+        public static int TestAccuracy(int accuracy, bool clear = false)
         {
             int probability = Helpful.Utility.GenerateRandomNumber(0, 101);
             if (probability >= 92)
@@ -22,9 +22,10 @@ namespace Combat
             else
             {
                 Helpful.Utility.WriteTimeClear("It misses!\n", 3000);
-                Helpful.Utility.WriteTimeClear("Nothing more happens!", 2000, false, true);
+                Helpful.Utility.WriteTimeClear("Nothing else happens!", 2000, false, true);
                 return 0;
             }
+            if (clear) { Console.Clear(); }
         }
         public static void attackWithEffects(Entities.Entity caster, Entities.Entity target, int currentAttack)
         {
@@ -47,11 +48,6 @@ namespace Combat
                     Helpful.Utility.WriteTimeClear($"It hits {timesHitted} more times, causing {timesHitted * caster.AttackList[currentAttack].attackDamage} points of damage", 3000, false, true);
                     target.CurrentHealth -= 20;
                     target.Affected = false;
-                }
-                else if (creatureAttackEffect.effectType == 4)
-                {
-                    caster.Stun = 1;
-                    caster.Casting = true;
                 }
             }
         }
@@ -93,7 +89,7 @@ namespace Combat
                     attackDicctionary.Add(numOptions.ToString(), attack);
                     numOptions++;
                 }
-                Console.WriteLine($"{numOptions} - Info");
+                Console.WriteLine($"{numOptions} - Info\n{numOptions+1} - Block");
                 string playerAction = Console.ReadLine();
                 if (attackDicctionary.ContainsKey(playerAction))
                 {
@@ -132,41 +128,36 @@ namespace Combat
                     if (attackDicctionary.ContainsKey(requiredInfo)) { Helpful.Utility.WriteTimeClear($"\n{attackDicctionary[requiredInfo].attackName}: You {attackDicctionary[requiredInfo].attackDescription}\nAccuracy - {attackDicctionary[requiredInfo].attackAccuracy}\nDamage - {attackDicctionary[requiredInfo].attackDamage}", 5000, false, true); }
                     else { Helpful.Utility.WriteTimeClear("Character not recognized!", 2000, false, true); }
                 }
+                else if (playerAction == (numOptions + 1).ToString()) { Helpful.Utility.WriteTimeClear("Player's action:\n\nYou assume a defensive stance.", 2500, true); player.Block = true; turn++; }
                 else { Helpful.Utility.WriteTimeClear("Character not recognized!", 2000, false, true); }
 
                 if (enemy.CurrentHealth > 0 && turn == 1)
                 {
-                    enemy.Block = false;
                     Helpful.Utility.WriteTimeClear($"{enemy.Name}'s action:\n", 0, true);
-                    if (enemy.Casting)
-                    {
-                        if (enemy.Affected == false)
-                        {
-                            int enemyPrecision = TestAccuracy(enemy.AttackList[enemyCurrentAttack].attackAccuracy);
-                            Helpful.Utility.WriteTimeClear($"The enemy releases it, causing {enemy.AttackList[enemyCurrentAttack].attackDamage * enemyPrecision} ", 3000);
-                            enemy.Casting = false;
-                        }
-                        else { Helpful.Utility.WriteTimeClear("The player cancelled the cast!", 2000); enemy.Casting = false; }
-                    }
-                    if (enemy.Affected && enemy.AttackList[enemyCurrentAttack].attackName != "Arcane Crush")
+                    enemy.Block = false;
+                    if (enemy.Affected)
                     {
                         Helpful.Utility.WriteTimeClear($"The enemy was affected by {player.AttackList[Convert.ToInt32(playerAction)-1].attackName}", 3000);
                         lowerEffectTime(enemy);
                         Helpful.Utility.WriteTimeClear("Nothing else happens.", 2000, false, true);
-                    enemyCurrentAttack = Helpful.Utility.GenerateRandomNumber(0, enemy.AttackList.Count);
                     }
                     else
                     {
-                        int enemyPrecision = TestAccuracy(enemy.AttackList[enemyCurrentAttack].attackAccuracy);
-                        Helpful.Utility.WriteTimeClear($"It {enemy.AttackList[enemyCurrentAttack].attackDescription}", 3000);
-                        if (enemyPrecision > 0)
+                        enemyCurrentAttack = Helpful.Utility.GenerateRandomNumber(0, enemy.AttackList.Count+1);
+                        if (enemyCurrentAttack == enemy.AttackList.Count) { Helpful.Utility.WriteTimeClear("It assumes a defensive stance.", 2500, false, true); enemy.Block = true; }
+                        else
                         {
-                            if (player.Block) { Helpful.Utility.WriteTimeClear("The player was blocking!\nIt deals 0 points of damage", 3000, false, true); }
-                            else
+                            Helpful.Utility.WriteTimeClear($"It {enemy.AttackList[enemyCurrentAttack].attackDescription}", 3000);
+                            int enemyPrecision = TestAccuracy(enemy.AttackList[enemyCurrentAttack].attackAccuracy, true);
+                            if (enemyPrecision > 0)
                             {
-                                Helpful.Utility.WriteTimeClear($"The enemy causes {enemy.AttackList[enemyCurrentAttack].attackDamage * enemyPrecision} points of damage", 3000, false, true);
-                                player.CurrentHealth -= enemy.AttackList[enemyCurrentAttack].attackDamage * enemyPrecision;
-                                attackWithEffects(enemy, player, enemyCurrentAttack);
+                                if (player.Block) { Helpful.Utility.WriteTimeClear("The player was blocking!\nIt deals 0 points of damage", 3000, false, true); }
+                                else
+                                {
+                                    Helpful.Utility.WriteTimeClear($"The enemy causes {enemy.AttackList[enemyCurrentAttack].attackDamage * enemyPrecision} points of damage", 3000, false, true);
+                                    player.CurrentHealth -= enemy.AttackList[enemyCurrentAttack].attackDamage * enemyPrecision;
+                                    attackWithEffects(enemy, player, enemyCurrentAttack);
+                                }
                             }
                         }
                     }
